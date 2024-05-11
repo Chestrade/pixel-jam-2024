f134aa965185ed011795240b9827a79bd01860ec
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2024-05-11 17:04:45 -0400

    Added player controller sinking and inertia

--- Scripts/player_blowfish.gd
+++ Scripts/player_blowfish.gd
@@ -2,29 +2,65 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var screen_size
+var direction: Vector2
+var is_move_input_received: bool = false
+var is_sinking: bool = false
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+var inertia: float = 0.95
 
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if (Input.get_vector("swim_left","swim_right","swim_up","swim_down"))==Vector2.ZERO:
-		velocity.y = 0
-
-	# Handle jump.
-	if Input.is_action_just_pressed("swim_up"):
-		velocity.y = JUMP_VELOCITY
-	elif Input.is_action_just_pressed("swim_down"):
-		velocity.y = -JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("swim_left", "swim_right")
-	if direction:
-		velocity.x = direction * SPEED
+	get_input()
+	
+	if is_move_input_received:
+		velocity = velocity * inertia
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity = direction.normalized() * SPEED
+	
+	print("Velocity: ", velocity)
+	print("Direction: ", direction)
+	
+	if is_sinking:
+		velocity.y = gravity * delta
+	
+	# Flip sprite according to direction of movement
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	
+	# Prevent player from leaving screen
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
 	move_and_slide()
+
+func get_input():
+	direction = Vector2.ZERO
+	# Calculate movement vector
+	if Input.is_action_pressed("swim_right"):
+		direction.x = 1
+	if Input.is_action_pressed("swim_left"):
+		direction.x = -1
+	if Input.is_action_pressed("swim_down"):
+		direction.y = 1
+	if Input.is_action_pressed("swim_up"):
+		direction.y = -1
+	
+	# If player is not moving, slowly sink down
+	if !Input.is_action_pressed("swim_up") and \
+	   !Input.is_action_pressed("swim_down") and \
+	   !Input.is_action_pressed("swim_left") and \
+	   !Input.is_action_pressed("swim_right"):
+		is_move_input_received = true
+		
+		if $SinkTimer.is_stopped():
+			$SinkTimer.start()
+	else:
+		$SinkTimer.stop()
+		is_sinking = false
+		is_move_input_received = false
+
+func _on_sink_timer_timeout() -> void:
+	is_sinking = true
