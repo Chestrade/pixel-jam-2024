8a29d8cc957493d9c204116da773fb6086023fa1
Anton <76670672+Chestrade@users.noreply.github.com>
2024-05-11 15:48:27 -0400

    Angler fish wander and chase states

--- Scripts/angler_fish.gd
+++ Scripts/angler_fish.gd
@@ -1,39 +1,66 @@
 extends CharacterBody2D
 
-@export var speed = 50
+@export var speed = 5000
 @export var nav_agent : NavigationAgent2D
+
+@export var markers : Array[Marker2D] = []
+var currentMarkerIndex = -1
+var isReachingTarget : bool = false
 
 var target_node = null
 var home_pos = Vector2.ZERO
 
+enum STATE {WANDERING, CHASE}
+var currentState : STATE
+
 func _ready() -> void:
+	SetState(STATE.WANDERING)
 	home_pos = self.global_position
-	
 	nav_agent.path_desired_distance = 4
 	nav_agent.target_desired_distance = 4
 
 func _physics_process(delta: float) -> void:
-	if nav_agent.is_navigation_finished():
+	match currentState:
+		STATE.WANDERING:
+			if isReachingTarget:
+				if nav_agent.is_navigation_finished():
+					isReachingTarget = false
+					currentMarkerIndex = (currentMarkerIndex + 1) % markers.size()
+					nav_agent.target_position = markers[currentMarkerIndex].global_position
+					
+			else:
+				if nav_agent.is_navigation_finished():
+					isReachingTarget = true
+					nav_agent.target_position = markers[currentMarkerIndex].global_position
+					
+		STATE.CHASE:
+			nav_agent.target_position = target_node.global_position
+	
+	if nav_agent.is_navigation_finished(): #if the nav agent reached its target
 		return
-		
-	var axis = to_local(nav_agent.get_next_path_position()).normalized()
-	velocity = axis * speed
+	var axis : Vector2 = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = axis * speed * delta
 	move_and_slide()
 
+func SetState(newState : STATE) -> void:
+	currentState = newState
+
 func recalc_path() -> void:
-	if target_node:
+	if target_node and currentState == STATE.CHASE:
 		nav_agent.target_position = target_node.global_position
-	else:
-		nav_agent.target_position = home_pos
+	elif currentState == STATE.WANDERING:
+		nav_agent.target_position = markers[currentMarkerIndex].global_position
 
 func _on_timer_timeout() -> void:
 	recalc_path()
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	SetState(STATE.CHASE)
 	target_node = area.owner
 
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	if area.owner == target_node:
 		target_node = null
+	SetState(STATE.WANDERING)
