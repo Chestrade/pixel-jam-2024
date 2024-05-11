e9ff81a7674cda2040742a88b3d4d1e9dc8ea434
Anton <76670672+Chestrade@users.noreply.github.com>
2024-05-11 14:52:22 -0400

    Angler fish follows player

--- Scripts/angler_fish.gd
+++ Scripts/angler_fish.gd
@@ -1,62 +1,39 @@
 extends CharacterBody2D
 
-@export var patrolSpeed : float = 300.0
-@export var chaseSpeed : float = 350.0
+@export var speed = 50
+@export var nav_agent : NavigationAgent2D
 
-@onready var nav : NavigationAgent2D = $NavigationAgent2D
-@onready var animationPlayer : AnimationPlayer = $AnglerFishAnimationPlayer
-
-enum AnglerState {WANDERING, ALERT, CHASE, EAT}
-var currentState : AnglerState
+var target_node = null
+var home_pos = Vector2.ZERO
 
 func _ready() -> void:
-	setState(AnglerState.WANDERING)
+	home_pos = self.global_position
+	
+	nav_agent.path_desired_distance = 4
+	nav_agent.target_desired_distance = 4
 
 func _physics_process(delta: float) -> void:
-	UpdateState(delta)
-
-func setState(newState : AnglerState) -> void:
-	if(currentState == newState):
+	if nav_agent.is_navigation_finished():
 		return
-	
-	ExitState()
-	currentState = newState
-	EnterState()
-
-func EnterState() -> void: #What happens when the angler fish enters a state
-	match currentState:
-		AnglerState.WANDERING:
-			animationPlayer.play("wandering")
-		AnglerState.ALERT:
-			animationPlayer.play("alert")
-		AnglerState.CHASE:
-			animationPlayer.play("chase")
-		AnglerState.EAT:
-			animationPlayer.play("eat")
-
-func ExitState() -> void: # What happens when the anglerfish exits a state
-	match currentState:
-		AnglerState.WANDERING:
-			pass
-		AnglerState.ALERT:
-			pass
-		AnglerState.CHASE:
-			pass
-		AnglerState.EAT:
-			pass
 		
+	var axis = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = axis * speed
+	move_and_slide()
 
-func UpdateState(delta : float) -> void: #Manages state behaviour
-	match currentState:
-		AnglerState.WANDERING: #Wandering State behavior
-			pass
-		AnglerState.ALERT: # Alert State Behaviour
-			pass
-		AnglerState.CHASE: # Chase State Behaviour
-			pass
-		AnglerState.EAT: # Eat State Behaviour
-			pass
-	
+func recalc_path() -> void:
+	if target_node:
+		nav_agent.target_position = target_node.global_position
+	else:
+		nav_agent.target_position = home_pos
+
+func _on_timer_timeout() -> void:
+	recalc_path()
 
 
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	target_node = area.owner
 
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.owner == target_node:
+		target_node = null
