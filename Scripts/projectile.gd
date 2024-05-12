373cde120f8b97253f44d9dffc3259f2a2236eb8
Anton <76670672+Chestrade@users.noreply.github.com>
2024-05-12 16:31:58 -0400

    Added light occluder on projectile

--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -17,13 +17,11 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Detection Area"):
-		print("Projectile detected body with group : Detection Area")
 		return
 	else:
-		print("Projectile destroyed on impact" + body.name)
+		print(body.name)
 		queue_free()
 
 
 func _on_life_timeout() -> void:
 	queue_free()
-	print("Projectile kaput.")
