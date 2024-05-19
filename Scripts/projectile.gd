cea206d17d326e657279f8dd67a9230141e83b0f
Chestrade <76670672+Chestrade@users.noreply.github.com>
2024-05-18 20:29:12 -0400

    added particules and audio structure to projectile
    The projectile doesn't have sound yet

--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -6,6 +6,8 @@ var dir : float
 var spawnPosition : Vector2
 var spawnRotation : float
 
+@onready var explosion_particules : GPUParticles2D = $"Explosion particules"
+@onready var audio_player : AudioStreamPlayer2D = $"Impact SFX"
 func _ready() -> void:
 	global_position = spawnPosition
 	global_rotation = spawnRotation
@@ -14,6 +16,9 @@ func _physics_process(_delta: float) -> void:
 	velocity = Vector2(0, -speed).rotated(dir)
 	move_and_slide()
 
+func emit_particules() -> void: # used to emit particules at the end of the node's life
+	explosion_particules.set_emitting(true)
+	audio_player.play()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Detection Area"):
@@ -21,8 +26,10 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	else:
 		if body.name == "Player":
 			body.takeDamage(25)
-		# print(body.name, " was hit")
+		emit_particules()
 		queue_free()
 
 func _on_life_timeout() -> void:
+	emit_particules()
 	queue_free()
+	
